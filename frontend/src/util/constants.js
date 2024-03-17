e79e14acb9ac8b55a07f4e26aa50aba2635ab908
Kuba Wysocki <jakub.wysocki.it@gmail.com>
2024-03-17 18:55:34 +0100

    set same origin for production

--- frontend/src/util/constants.js
+++ frontend/src/util/constants.js
@@ -1,4 +1,4 @@
-export const BASE_URL = 'http://localhost:8000'
+export const BASE_URL = process.env.NODE_ENV === 'production' ? '' : 'http://localhost:8000'
 export const LS_TOKEN = {
   KEY: 'TOKEN',
   PRESENT: 'PRESENT',
